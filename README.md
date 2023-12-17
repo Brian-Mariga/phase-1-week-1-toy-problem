6cdab83924f9d0bb3feeb8f0b236674b6a272955
Brian <brianmariga0@gmail.com>
2023-12-18 00:08:41 +0300

    Completed Assignment

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # phase-1-week-1-toy-problem
+`Challenge 1: Student Grade Generator (Toy Problem)`
+    *Overview*
+    *The JavaScript function "grading()" is designed to calculate the grade of a student based on the marks attained by the student.*
+    
+    1.The function "grading()" for calculating a student's grade is declared using the "Function" keyword
+
+    2.When the code is runned, the user is prompted to input the marks of a student for grading.
+
+    3.The function "Number()" converts the input to a numeric value before  the calculations.
+
+    4.The function "isNaN" check if the input is a number. If the input is not a number ,it tells the user to enter a number else it procceds.
+
+    5.The input is then checked if it is from 0 to 100. It is not in the range it tells the user to enter a valid marks.
+
+    6.The function uses multiple "if" statements to compare the marks to different ranges and return the corresponding grade string using the following criteria:
+        A > 79, B - 60 to 79, C -  59 to 49, D - 40 to 49, E - less 40.
+
+    7.The final "else" statement handles any remaining cases and returns "Grade is E".
+
+    8.The function then outputs the Grade based on the marks entered.
+
+
+`Challenge 2: Speed Detector (Toy Problem)`
+    *Overview*
+    *The JavaScript function "calculateDemerits()" is designed to calculate demerit points based on the speed of a vehicle exceeding a specified speed limit. It also checks if the accumulated demerit points lead to the suspension of the driver's license.*
+
+    1.The function "calculateDemerits()" for calculating the demerits points of a driver is declared using the "Function" keyword
+
+    2.The constant speed limit and maximum demerit points are set.
+
+    3.The function "isNaN" check if the input is a number. If the input is not a number ,it tells the user to enter a number else, it procceds.
+
+    4. If the speed entered is less then or equal to the speed limit, the driver is okay.
+
+    5.Incase the speed is greater than the speed limit, the demerit points are calculated.
+
+    7.The demerit points are compared to the maximum demerit points an if they exceed the maximum demerit points, it outputs that the driver's licence is suspended.
+
+`Challenge 3: Net Salary Calculator (Toy Problem)`
+    *Overview*
+
--- netsalarycalculator.js
+++ netsalarycalculator.js
@@ -1,17 +1,73 @@
-
+// Function to calculate tax based on KRA rates
 function calculatePayee(grossSalary) {
-    if (grossSalary <= 24000) {
-      return 0;
-    } else if (grossSalary <= 32000) {
-      return (grossSalary - 24000) * 0.1;
-    } else if (grossSalary <= 41000) {
-      return (grossSalary - 32000) * 0.15 + 800;
-    } else if (grossSalary <= 52000) {
-      return (grossSalary - 41000) * 0.20 + 2,100;
-    } else if (grossSalary <= 63000) {
-      return (grossSalary - 52000) * 0.25 + 4,100;
-    } else {
-      return (grossSalary - 63000) * 0.30 + 8,600;
-    }
+  if (grossSalary <= 24000) {
+    return 0;
+  } else if (grossSalary <= 32000) {
+    return (grossSalary - 24000) * 0.1;
+  } else if (grossSalary <= 41000) {
+    return (grossSalary - 32000) * 0.15 + 800;
+  } else if (grossSalary <= 52000) {
+    return (grossSalary - 41000) * 0.20 + 2,100;
+  } else if (grossSalary <= 63000) {
+    return (grossSalary - 52000) * 0.25 + 4,100;
+  } else {
+    return (grossSalary - 63000) * 0.30 + 8,600;
   }
-calculatePayee(20000)
+}
+
+// Function to calculate NHIF deductions
+function calculateNhifDeductions(grossSalary) {
+  if (grossSalary <= 5,999.99) {
+    return 150;
+  } else if (grossSalary <= 14,999.99) {
+    return 300;
+  } else if (grossSalary <= 24,999.99) {
+    return 400;
+  } else if (grossSalary <= 34,999.99) {
+    return 550;
+  } else if (grossSalary <= 44,999.99) {
+    return 600;
+  } else if (grossSalary <= 54,999.99) {
+    return 800;
+  } else if (grossSalary <= 64,999.99) {
+    return 1000;
+  } else if (grossSalary <= 74,999.99) {
+    return 1200;
+  } else if (grossSalary <= 84,999.99) {
+    return 1400;
+  } else {
+    return 1500;
+  }
+}
+
+// Function to calculate NSSF deductions
+function calculateNssfDeductions(grossSalary) {
+  if (grossSalary <= 5,999.99) {
+    return 0;
+  } else if (grossSalary <= 6,999.99) {
+    return 200;
+  } else if (grossSalary <= 7,999.99) {
+    return 300;
+  } else if (grossSalary <= 8,999.99) {
+    return 400;
+  } else if (grossSalary <= 9,999.99) {
+    return 500;
+  } else if (grossSalary <= 10,999.99) {
+    return 600;
+  } else if (grossSalary <= 11,999.99) {
+    return 700;
+  } else if (grossSalary <= 12,999.99) {
+    return 800;
+  } else if (grossSalary <= 13,999.99) {
+    return 900;
+  } else if (grossSalary <= 14,999.99) {
+    return 1000;
+  } else if (grossSalary <= 15,999.99) {
+    return 1,100;
+  } else if (grossSalary <= 16,999.99) {
+    return 1,200;
+  } else if (grossSalary <= 17,999.99) {
+    return 1,300;
+  } else if (grossSalary <= 18,999.99) {
+    return 1,400;
+  
